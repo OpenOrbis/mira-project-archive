1c1652d697a238e11ccb9ae586077a8aec22acb6
Vultra <30537108+Vultra@users.noreply.github.com>
2018-05-20 13:40:27 +0100

    Updated Copy in/out offset for 5.01

--- Firmware/MiraFW/src/mira/boot/patches/patches501.c
+++ Firmware/MiraFW/src/mira/boot/patches/patches501.c
@@ -36,8 +36,8 @@ void install_prerunPatches_501()
 	kmem[0] = 0x07;
 
 	// Patch copy(in/out) ?
-	//uint16_t *copyinpatch = (uint16_t*)&gKernelBase[0x1EA657];
-	//uint16_t *copyoutpatch = (uint16_t*)&gKernelBase[0x1EA572];
+	//uint16_t *copyinpatch = (uint16_t*)&gKernelBase[0x1EA600];
+	//uint16_t *copyoutpatch = (uint16_t*)&gKernelBase[0x1EA520];
 
 	// Enable MAP_SELF
 	kmem = (uint8_t*)&gKernelBase[0x117b0];
