456f3b04e20b259c4981fdafa857653e8d7dd636
kiwidog <forums@allenthinks.com>
2018-06-30 14:06:35 -0700

    Git sucks

---
+++ Tools/MiraToolkit/Controls/Debugger/ucCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using MiraToolkit.Core.Debugger;
+
+namespace MiraToolkit.Controls.Debugger
+{
+    public partial class ucCommand : UserControl
+    {
+        private MiraDebugger m_Debugger;
+
+        public ucCommand(MiraDebugger p_Debugger)
+        {
+            InitializeComponent();
+
+            m_Debugger = p_Debugger;
+        }
+    }
+}
---
+++ Tools/MiraToolkit/Controls/Debugger/ucProcess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiraToolkit.Controls.Debugger
+{
+    public partial class ucProcess : UserControl
+    {
+        public ucProcess()
+        {
+            InitializeComponent();
+        }
+    }
+}
