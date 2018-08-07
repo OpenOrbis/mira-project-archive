c1689dc2444308e6f4b0e4ac285a5335653fb811
TheoryWrong <theorywrong311@gmail.com>
2018-08-07 09:28:20 +0200

    OverlayFS changes

--- Firmware/MiraFW/src/mira/fs/overlay/overlayfs.c
+++ Firmware/MiraFW/src/mira/fs/overlay/overlayfs.c
@@ -17,18 +17,21 @@
 
 #include <mira/miraframework.h>
 
-
+#include <sys/filedesc.h>				// filedesc
 #include <oni/utils/cpu.h>
 
+struct alt_filedesc {
+	void *useless1[3];
+	void *fd_rdir;
+	void *fd_jdir;
+};
+
 int overlayfs_sys_open(struct thread* td, struct open_args* uap);
-static void
-build_iovec(struct iovec **iov, int *iovlen, const char *name, void *val,
-	size_t len);
+
+static void build_iovec(struct iovec **iov, int *iovlen, const char *name, void *val, size_t len);
 
 // Taken directly from sbin/mount/getmntopts.c
-static void
-build_iovec(struct iovec **iov, int *iovlen, const char *name, void *val,
-	size_t len)
+static void build_iovec(struct iovec **iov, int *iovlen, const char *name, void *val, size_t len)
 {
 	size_t (*strlen)(const char *str) = kdlsym(strlen);
 	char * (*strdup)(const char *string, struct malloc_type* type) = kdlsym(strdup);
@@ -65,27 +68,16 @@ build_iovec(struct iovec **iov, int *iovlen, const char *name, void *val,
 	*iovlen = ++i;
 }
 
-
-static int mount_fs(struct thread* td, char* device, char* mountpoint, char* fstype, char* mode, unsigned int flags)
+static int mount_nullfs(struct thread* td, char* target, char* source, unsigned int flags)
 {
 	struct iovec* iov = NULL;
 	int iovlen = 0;
 
-	WriteLog(LL_Debug, "here");
+	WriteLog(LL_Debug, "NullFS here !");
 
-	build_iovec(&iov, &iovlen, "fstype", fstype, -1);
-	build_iovec(&iov, &iovlen, "fspath", mountpoint, -1);
-	build_iovec(&iov, &iovlen, "from", device, -1);
-	build_iovec(&iov, &iovlen, "large", "yes", -1);
-	build_iovec(&iov, &iovlen, "timezone", "static", -1);
-	build_iovec(&iov, &iovlen, "async", "", -1);
-	build_iovec(&iov, &iovlen, "ignoreacl", "", -1);
-
-	if (mode) 
-	{
-		build_iovec(&iov, &iovlen, "dirmask", mode, -1);
-		build_iovec(&iov, &iovlen, "mask", mode, -1);
-	}
+	build_iovec(&iov, &iovlen, "fstype", "nullfs", (size_t)-1);
+	build_iovec(&iov, &iovlen, "fspath", source, (size_t)-1); // Ou je veut le mettre
+	build_iovec(&iov, &iovlen, "target", target, (size_t)-1); // Se que je veut mettre
 
 	int(*nmount)(struct thread* thread, struct nmount_args*) = kdlsym(sys_nmount);
 	if (!nmount)
@@ -110,15 +102,57 @@ static int mount_fs(struct thread* td, char* device, char* mountpoint, char* fst
 	return td->td_retval[0];
 }
 
+static int mount_unionfs(struct thread* td, char* target, char* source, unsigned int flags)
+{
+	struct iovec* iov = NULL;
+	int iovlen = 0;
+
+	WriteLog(LL_Debug, "here");
+
+	build_iovec(&iov, &iovlen, "fstype", "unionfs", (size_t)-1);
+	build_iovec(&iov, &iovlen, "fspath", source, (size_t)-1); // Ou je veut le mettre
+	build_iovec(&iov, &iovlen, "from", target, (size_t)-1); // Se que je veut mettre
+	build_iovec(&iov, &iovlen, "ro", "", (size_t)-1);
+
+	int(*nmount)(struct thread* thread, struct nmount_args*) = kdlsym(sys_nmount);
+	if (!nmount)
+		return -1;
+
+	int error;
+	struct nmount_args uap;
+
+	// clear errors
+	td->td_retval[0] = 0;
+
+	// call syscall
+	uap.iovp = iov;
+	uap.iovcnt = iovlen;
+	uap.flags = flags;
+
+	error = nmount(td, &uap);
+	if (error)
+		return -error;
+
+	// return socket
+	return td->td_retval[0];
+}
 
 int overlayfs_onExecNewVmspace(struct image_params* imgp, struct sysentvec* sv);
 
+// 5.05 Stuff
+#define kdlsym_addr_exec_new_vmspace  0x0038AD10
+#define kdlsym_addr_kern_mkdirat 0x00340BD0
+#define kdlsym_addr_strcmp 0x001D0FD0
+
+/*
+// 5.01 Stuff
 #define kdlsym_addr_dmem_start_app_process 0x002468E0
 #define kdlsym_addr_exec_new_vmspace 0x0038A940
 #define kdlsym_addr_kern_open 0x0072AB50
 #define kdlsym_addr_kern_openat 0x0033B640
 #define kdlsym_addr_kern_readv 0x00152A10
 #define kdlsym_addr_kern_close  0x000C0F40
+*/
 
 void overlayfs_init(struct overlayfs_t* fs)
 {
@@ -133,13 +167,30 @@ void overlayfs_init(struct overlayfs_t* fs)
 	fs->pid = -1;
 }
 
-int(*_sys_open)(struct thread* td, struct open_args* uap) = NULL;
+/*
+struct proc *proc_find_by_name(char* name) {
+	struct proc *p;
+
+	int (*strcmp)(const char *s1, const char *s2) = kdlsym(strcmp);
+	p = *(struct proc **)kdlsym(allproc);
+
+	do {
+		if (strcmp(p->p_comm, name) == 0) {
+			return p;
+		}
+	} while ((p = p->p_list.le_next));
+
+	return NULL;
+}
+*/
 
 int overlayfs_onExecNewVmspace(struct image_params* imgp, struct sysentvec* sv)
 {
 	char* (*strstr)(const char*, const char*) = kdlsym(strstr);
-	void(*critical_enter)(void) = kdlsym(critical_enter);
-	void(*critical_exit)(void) = kdlsym(critical_exit);
+	int (*kern_mkdirat)(struct thread *td, int fd, char *path, enum uio_seg segflg, int mode) = kdlsym(kern_mkdirat);
+
+	//void(*critical_enter)(void) = kdlsym(critical_enter);
+	//void(*critical_exit)(void) = kdlsym(critical_exit);
 
 	//
 	//	This is purely for updating the current drive and mount point
@@ -156,7 +207,8 @@ int overlayfs_onExecNewVmspace(struct image_params* imgp, struct sysentvec* sv)
 
 	hook_enable(fs->execNewVmspaceHook);
 
-	// We only want to catch eboots
+	WriteLog(LL_Debug, "ExecNewVmspace called !\n");
+
 	if (strstr(imgp->execpath, "eboot") == NULL)
 	{
 		WriteLog(LL_Debug, "%s not eboot", imgp->execpath);
@@ -166,110 +218,55 @@ int overlayfs_onExecNewVmspace(struct image_params* imgp, struct sysentvec* sv)
 	struct thread* td = imgp->proc->p_singlethread ? imgp->proc->p_singlethread : imgp->proc->p_threads.tqh_first;
 	int32_t pid = td->td_proc->p_pid;
 
+	struct ucred* cred = td->td_proc->p_ucred;
+	struct filedesc* fd = td->td_proc->p_fd;
+
+	// Escalate privileges
+	cred->cr_uid = 0;
+	cred->cr_ruid = 0;
+	cred->cr_rgid = 0;
+	cred->cr_groups[0] = 0;
+
+	// Game Escape sandbox
+	void* old_r_dir = fd->fd_rdir;
+	cred->cr_prison = *(void**)kdlsym(prison0);
+	fd->fd_rdir = fd->fd_jdir = *(void**)kdlsym(rootvnode);
+
 	WriteLog(LL_Debug, "Process Thread td %p pid %d", td, pid);
 
-	// This may get called for each thread created, so we skip if it's the same pid
-	if (fs->pid == pid)
-		return result;
+	// Ont créer le dossier CUSA00265_MOD
 
-	// Update the pid
-	fs->pid = pid;
-
-	// Create a new directory to mount this path at
-	int32_t mkdirResult = kmkdir_t("/mnt/sandbox/CUSA08034_000/usb0", 0777, mira_getMainThread());
-	WriteLog(LL_Debug, "mkdir returned %d", mkdirResult);
-	if (mkdirResult < 0)
+	int32_t mkdirResult = kern_mkdirat(td, AT_FDCWD, "/mnt/CUSA00265_MOD", UIO_SYSSPACE, 0777);
+	if (mkdirResult > 0)
 	{
-		if (mkdirResult != -EEXIST)
-		{
-			WriteLog(LL_Error, "could not create usb0 folder (%d)", mkdirResult);
+		if (mkdirResult != EEXIST) {
+			WriteLog(LL_Error, "could not create folder ! (%d)", mkdirResult);
 			return result;
+		} else {
+			WriteLog(LL_Debug, "Folder already exist, try anyway ...\n");
 		}
 	}
 
-	// Mount the usb device to the newly created path, exfatfs, force mount
-	int mountResult = mount_fs(td, "/dev/da1s1", "/mnt/sandbox/CUSA08034_000/usb0", "exfatfs", "511", MNT_FORCE);
-	WriteLog(LL_Debug, "nmount returned: %d", mountResult);
+	// Mount the folder CUSA00265_MOD with /mnt/sandbox/pfsmnt/CUSA00265-app0-patch0-union (Origin) in nullfs
+	int mountResult = mount_nullfs(td, "/mnt/sandbox/pfsmnt/CUSA00265-app0-patch0-union", "/mnt/CUSA00265_MOD", MNT_FORCE);
 	if (mountResult < 0)
 	{
-		WriteLog(LL_Warn, "could not mount usb0 to /mnt/usb0 (%d)", mountResult);
+		WriteLog(LL_Warn, "could not mount with NullFS ! (%d)", mountResult);
 		return result;
 	}
 
-	WriteLog(LL_Debug, "overwriting sys_open from %p to %p", sv->sv_table[AUE_OPEN].sy_call, overlayfs_sys_open);
+	// Cause crash when you try to go in /mnt/sandbox/pfsmnt/CUSA00265-app0-patch0-union with the ftp :P
+	mountResult = mount_unionfs(td, "/mnt/usb0", "/mnt/sandbox/pfsmnt/CUSA00265-app0-patch0-union", 0);
+	if (mountResult < 0)
+	{
+		WriteLog(LL_Warn, "could not mount with UnionFS ! (%d)", mountResult);
+		return result;
+	}
 
-	_sys_open = (void*)sv->sv_table[AUE_OPEN].sy_call;
+	WriteLog(LL_Info, "UnionFS Re-mount ! (%d)", mountResult);
 
-	critical_enter();
-	cpu_disable_wp();
-
-	sv->sv_table[AUE_OPEN].sy_call = (void*)overlayfs_sys_open;
-
-	cpu_enable_wp();
-	critical_exit();
+	// Rewrote the good old dir
+	fd->fd_rdir = old_r_dir;
 
 	return result;
-}
-
-int overlayfs_sys_open(struct thread* td, struct open_args* uap)
-{
-	if (!_sys_open)
-	{
-		WriteLog(LL_Error, "HOLY FUCK THIS IS BAD");
-		return -1;
-	}
-
-	struct overlayfs_t* fs = mira_getFramework()->overlayfs;
-
-	void * (*memset)(void *s, int c, size_t n) = kdlsym(memset);
-	int(*snprintf)(char *str, size_t size, const char *format, ...) = kdlsym(snprintf);
-
-	int(*copyinstr)(const void *uaddr, void *kaddr, size_t len, size_t *done) = kdlsym(copyinstr);
-
-	// We need to copy the path from userland
-	char inputPath[OVERLAYFS_MAXPATH];
-	memset(inputPath, 0, sizeof(inputPath));
-
-	size_t lengthCopied = 0;
-	int copyResult = copyinstr(uap->path, inputPath, sizeof(inputPath), &lengthCopied);
-	if (copyResult != 0)
-	{
-		WriteLog(LL_Error, "could not copyinstr path (%d)", copyResult);
-		return _sys_open(td, uap);
-	}
-
-	// Format the redirected path
-	char redirPath[OVERLAYFS_MAXPATH];
-	memset(redirPath, 0, sizeof(redirPath));
-	
-	snprintf(redirPath, sizeof(redirPath), "%s%s", "/mnt/sandbox/CUSA08034_000/usb0", inputPath);
-
-	if (!overlayfs_fileExists(fs, redirPath))
-		return _sys_open(td, uap);
-
-	WriteLog(LL_Debug, "attempting to open (%s) for replacement", redirPath);
-	uap->path = redirPath;
-		
-	return  _sys_open(td, uap);
-}
-
-uint8_t overlayfs_fileExists(struct overlayfs_t* fs, char* path)
-{
-	// Verify arguments
-	if (!fs || !path)
-		return false;
-
-	struct thread* td = mira_getMainThread();
-	if (!td)
-		return false;
-
-	// Attempt to open the file for read only
-	int fd = kopen_t(path, O_RDONLY, 0, td);
-	if (fd < 0)
-		return false;
-
-	// Close the handle
-	kclose_t(fd, td);
-
-	return true;
 }
